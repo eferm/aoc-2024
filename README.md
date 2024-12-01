603c775c54d656f2febeccbfe5905f7266a0d797
Emanuel Ferm <emanuel@eferm.com>
2024-12-01 23:45:30 +0000

    fetch inputs to separate dir

--- README.md
+++ README.md
@@ -1,22 +1,26 @@
 ## Setup
+
 Copy session cookie from browser to `.env` as `SESSION`.
+
+Load the env vars with one of:
+
+- In VSCode set `"python.experiments.optInto": ["pythonTerminalEnvVarActivation"]`
+- Run Python scripts with `uv run --env-file .env`
+- Prefix commands with `env $(cat .env | xargs)`
 
 ## Run
 
 Download and bootstrap new day's puzzle:
 
 ```shell
-uv run --env-file .env src/cli.py 1
-```
-
-```shell
-env $(cat .env | xargs) bootstrap 1
+uv run src/utils.py 1
+bootstrap 1
 ```
 
 Run a puzzle script:
 
 ```shell
-uv run src/day01/main.py
+uv run src/year_2024/day_01.py
 ```
 
 Or in VSCode: _> Python: Run Python File_.
--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,70 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+import argparse
+import os
+from pathlib import Path
+
+import requests
 
 
-if TYPE_CHECKING:
-    from collections.abc import Callable, Iterable
-    from typing import Any
+YEAR = 2024
+
+INPUT_URL = "https://adventofcode.com/{year}/day/{day}/input"
+
+BOILERPLATE = '''\
+from src.utils import get_input
 
 
-def lprint(iterable: Iterable[Any]) -> None:
-    for value in iterable:
-        print(str(value))
+inp = """\\
+co py
+pa ste
+ex ample
+"""
+inp = get_input({year}, {day})
+
+lines = inp.splitlines()
+print(*lines[:10], sep="\\n")
+'''
 
 
-def lzip(*iterables: Iterable[Any]) -> list[Any]:
-    return list(zip(*iterables, strict=True))
+src_file = "src/year_{year}/day_{day:02}.py"
+inp_file = "data/input_{year}-12-{day:02}.txt"
 
 
-def lmap[T](func: Callable[[T], Any], *iterables: Iterable[T]) -> list[Any]:
-    return list(map(func, *iterables))
+def get_input(year: int, day: int) -> str:
+    target = Path(inp_file.format(year=year, day=day))
+    if not target.exists():
+        contents = fetch_input(year, day)
+        target.write_text(contents)
+    return target.read_text()
+
+
+def fetch_input(year: int, day: int) -> str:
+    print(f"Fetching input for {year=} {day=} from server...")
+    resp = requests.get(
+        INPUT_URL.format(year=year, day=day),
+        headers={"Cookie": f"session={os.environ['SESSION']}"},
+        timeout=10,
+    )
+    resp.raise_for_status()
+    return resp.text
+
+
+def cli() -> None:
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("day", type=int)
+    parser.add_argument("-y", "--year", type=int, default=YEAR)
+
+    args = parser.parse_args()
+
+    target = Path(src_file.format(**vars(args)))
+    if not target.exists():
+        print(f"Seeding file {target}...")
+        target.parent.mkdir(exist_ok=True)
+        target.write_text(BOILERPLATE.format(**vars(args)))
+
+    get_input(**vars(args))
+
+
+if __name__ == "__main__":
+    cli()
